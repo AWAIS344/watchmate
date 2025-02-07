adffe9880d60cd712479b78eccd6d98135f30594
awais3344 <awaisalishah02@gmail.com>
2025-02-07 18:23:52 +0500

    Views Config

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from watchlist_app import models
 
 # Create your views here.
+
+def watchlist_view(request):
+    watchlist = models.Watchlist.objects.all()
+    context= {'watchlist': watchlist}
+    return render(request, 'watchlist_app/watchlist.html',context)
