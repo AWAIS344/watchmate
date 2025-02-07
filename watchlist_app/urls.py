3483f69f1a2de7c6a57e15a8014ef09db8f65b4b
awais3344 <awaisalishah02@gmail.com>
2025-02-07 20:43:46 +0500

    VIEWS UPDATIONS

--- watchlist_app/urls.py
+++ watchlist_app/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from watchlist_app.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("list/" ,watchlist_view,name="movie_list"),
+    path("list/<int:pk>", movie_detail, name="movie_detail"),
+    path("admin/", admin.site.urls),
 ]
--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from watchlist_app import models
+from django.http import JsonResponse
 
 # Create your views here.
 
 def watchlist_view(request):
-    watchlist = models.Watchlist.objects.all()
-    context= {'watchlist': watchlist}
-    return render(request, 'watchlist_app/watchlist.html',context)
+    watchlist = models.Movie.objects.all()
+    data={"watchlist":list(watchlist.values())}
+
+    return JsonResponse(data)
+
+ 
+def movie_detail(request,pk):
+    movie = models.Movie.objects.get(pk=pk)
+    data={"name":movie.title,"description":movie.about,"genre":movie.genre,}
+
+    return JsonResponse(data)
