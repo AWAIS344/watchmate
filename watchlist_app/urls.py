6cd4495cda9f3c00093276badcae5c5e75994462
awais3344 <awaisalishah02@gmail.com>
2025-02-08 18:28:32 +0500

    API VIEWS UPDATIONS

--- watchlist_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from watchlist_app.views import *
-
-urlpatterns = [
-    path("list/" ,watchlist_view,name="movie_list"),
-    path("list/<int:pk>", movie_detail, name="movie_detail"),
-    path("admin/", admin.site.urls),
-]
--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render
-from watchlist_app import models
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from watchlist_app import models
+# from django.http import JsonResponse
 
-# Create your views here.
+# # Create your views here.
 
-def watchlist_view(request):
-    watchlist = models.Movie.objects.all()
-    data={"watchlist":list(watchlist.values())}
+# def watchlist_view(request):
+#     watchlist = models.Movie.objects.all()
+#     data={"watchlist":list(watchlist.values())}
 
-    return JsonResponse(data)
+#     return JsonResponse(data)
 
  
-def movie_detail(request,pk):
-    movie = models.Movie.objects.get(pk=pk)
-    data={"name":movie.title,"description":movie.about,"genre":movie.genre,}
+# def movie_detail(request,pk):
+#     movie = models.Movie.objects.get(pk=pk)
+#     data={"name":movie.title,"description":movie.about,"genre":movie.genre,}
 
-    return JsonResponse(data)
+#     return JsonResponse(data)
