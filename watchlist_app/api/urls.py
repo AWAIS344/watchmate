a54380e99dd045b3ff4d78f1a308b083d7dfc575
awais3344 <awaisalishah02@gmail.com>
2025-02-10 23:52:28 +0500

    Serializer Bug Fixes

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.shortcuts import render
 
-from .views import watchlist,moviedetails
+from .views import MOVIELISTAV,MOVIEDETAILSAC
 
 urlpatterns = [
-    path("list/",watchlist,name="watchlist"),
-    path("list/<int:pk>",moviedetails,name="moviedetails"),
+    path("list/",MOVIELISTAV.as_view(),name="watchlist"),
+    path("list/<int:pk>",MOVIEDETAILSAC.as_view(),name="moviedetails"),
 ]
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,54 +1,102 @@
 from rest_framework import serializers
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from watchlist_app import models
 from .serializers import WatchSerializer
 
-@api_view(['GET','POST'])
-def watchlist(request):
+class MOVIELISTAV(APIView):
 
-    if request.method == 'POST':
-        serializer = WatchSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        movies = models.Movie.objects.all()
-        serializers = WatchSerializer(movies,many=True)
+    def get(self,request):
+        movie=models.Movie.objects.all()
+        serializers=WatchSerializer(movie,many=True)
         return Response(serializers.data)
+    
+    def post(self,request):
+        serializers=WatchSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
 
 
-@api_view(['GET','PUT','DELETE','PATCH'])
-def moviedetails(request,pk):
+class MOVIEDETAILSAC(APIView):
 
-    if request.method == 'GET':
-        movie = models.Movie.objects.get(pk=pk)
+    def get(self,request,pk):
+        movie=models.Movie.objects.get(pk=pk)
         serializer = WatchSerializer(movie)
 
         return Response(serializer.data)
-    if request.method == 'PUT':
-        movie = models.Movie.objects.get(pk=pk)
-        serializer = WatchSerializer(movie,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+
+
+    def put(self,request,pk):
+        movie =models.Movie.objects.get(pk=pk)
+        serializers=WatchSerializer(movie,data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
         return Response(status=status.HTTP_404_NOT_FOUND)
     
-
-    if request.method == 'DELETE':
-        movie = models.Movie.objects.get(pk=pk)
+    def delete(self,request,pk):
+        movie=models.Movie.objects.get(pk=pk)
         movie.delete()
         return Response({"message":"Movie deleted"})
     
-    if request.method=='PATCH':
+    def patch(self,request,pk):
         movie=models.Movie.objects.get(pk=pk)
         serializer=WatchSerializer()
         movie=serializer.toogle(movie)
 
         return Response({'message':"Status Updated Successfully","pulished":movie.published})
+
+    
+
+
+# @api_view(['GET','POST'])
+# def watchlist(request):
+
+#     if request.method == 'POST':
+#         serializer = WatchSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == 'GET':
+#         movies = models.Movie.objects.all()
+#         serializers = WatchSerializer(movies,many=True)
+#         return Response(serializers.data)
+
+
+# @api_view(['GET','PUT','DELETE','PATCH'])
+# def moviedetails(request,pk):
+
+#     if request.method == 'GET':
+#         movie = models.Movie.objects.get(pk=pk)
+#         serializer = WatchSerializer(movie)
+
+#         return Response(serializer.data)
+#     if request.method == 'PUT':
+#         movie = models.Movie.objects.get(pk=pk)
+#         serializer = WatchSerializer(movie,data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+    
+
+#     if request.method == 'DELETE':
+#         movie = models.Movie.objects.get(pk=pk)
+#         movie.delete()
+#         return Response({"message":"Movie deleted"})
+    
+#     if request.method=='PATCH':
+#         movie=models.Movie.objects.get(pk=pk)
+#         serializer=WatchSerializer()
+#         movie=serializer.toogle(movie)
+
+#         return Response({'message':"Status Updated Successfully","pulished":movie.published})
 
 
         
