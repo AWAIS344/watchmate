5aa99013fc5cf877b80dacdc9c64ffb84af5fa66
awais3344 <awaisalishah02@gmail.com>
2025-02-11 17:17:22 +0500

    Model Serializers

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -8,36 +8,44 @@ def validate_name(value):
     return value
 
 
-class WatchSerializer(serializers.Serializer):
-    id=serializers.IntegerField(read_only=True)
-    title = serializers.CharField(validate=[validate_name])
-    about = serializers.CharField()
-    genre = serializers.CharField()
-    year = serializers.IntegerField()
-    published = serializers.BooleanField()
+class WatchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Movie
+        fields="__all__"
 
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title',instance.title)
-        instance.about = validated_data.get('about',instance.about)
-        instance.genre = validated_data.get('genre',instance.genre)
-        instance.year = validated_data.get('year',instance.year)
-        instance.published = validated_data.get('published',instance.published)
-        instance.save()
-        return instance
-    
-    def toogle(self,instance):
-        instance.published=not instance.published
-        instance.save()
-        return instance
-    
     def validate(self,data):
         if data['title'] == data["about"]:
             raise serializers.ValidationError("Title and About Should not be SAME")
         return data
     
-    # def validate_title(self,value):
-    #     if len(value)<2:
-    #         raise serializers.ValidationError("Name too Short")
-    #     return value
+    def validate_title(self,value):
+        if len(value)<2:
+            raise serializers.ValidationError("Name too Short")
+        return value
+        
+
+
+    # id=serializers.IntegerField(read_only=True)
+    # title = serializers.CharField(validate=[validate_name])
+    # about = serializers.CharField()
+    # genre = serializers.CharField()
+    # year = serializers.IntegerField()
+    # published = serializers.BooleanField()
+
+    # def create(self, validated_data):
+    #     return Movie.objects.create(**validated_data)
+    # def update(self, instance, validated_data):
+    #     instance.title = validated_data.get('title',instance.title)
+    #     instance.about = validated_data.get('about',instance.about)
+    #     instance.genre = validated_data.get('genre',instance.genre)
+    #     instance.year = validated_data.get('year',instance.year)
+    #     instance.published = validated_data.get('published',instance.published)
+    #     instance.save()
+    #     return instance
+    
+    # def toogle(self,instance):
+    #     instance.published=not instance.published
+    #     instance.save()
+    #     return instance
+    
+    
