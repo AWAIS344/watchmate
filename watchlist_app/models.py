68cbe6c6f1aee67e42b687390edf0ac5af028bfe
awais3344 <awaisalishah02@gmail.com>
2025-02-16 21:06:54 +0500

    Model Addition

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator , MinValueValidator
 
 # Create your models here.
 
@@ -26,3 +27,14 @@ class WatchList(models.Model):
     def __str__(self):
         return self.title
     
+class Reviews(models.Model):
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment=models.CharField(max_length=100)
+    watchlist=models.ForeignKey(WatchList,on_delete=models.CASCADE,related_name='reviews')
+    active=models.BooleanField(default=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.rating) + " | " + self.watchlist.title
+
