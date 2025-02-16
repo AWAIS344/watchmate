2ad286d58f1ce256a0ffd9916cf48907f68a1326
awais3344 <awaisalishah02@gmail.com>
2025-02-16 21:10:04 +0500

    Model Addition

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
-from watchlist_app.models import WatchList,StreamPlatform
+from watchlist_app.models import WatchList,StreamPlatform,Reviews
 
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Reviews
+        fields="__all__"
 
 class WatchListSerializer(serializers.ModelSerializer):
     len_name=serializers.SerializerMethodField()
-
-
+    reviews=ReviewSerializer(many=True,read_only=True)
+    
     class Meta:
         model=WatchList
         fields="__all__"
