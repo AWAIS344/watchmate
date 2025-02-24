9f03e051bacf32b9b39d2dff99cb99f6f92814fa
awais3344 <awaisalishah02@gmail.com>
2025-02-25 02:44:37 +0500

    User Registration

--- user_app/api/serializers.py
+++ user_app/api/serializers.py
@@ -1,9 +1,26 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.serializers import ValidationError
+
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-
+    password2=serializers.CharField(style={"input_type":'password'} ,write_only=True)
     class Meta:
         model= User
         fields=["username",'email','password','password2']
+        extra_kwargs={
+            'password':{'write_only':True}
+        }
+
+    def save(self,):
+        password=self.validated_data["password"]
+        passwprd2=self.validated_data["password2"]
+
+        if password!=passwprd2:
+            raise serializers.ValidationError({'error':"P1 and P2 must Match"})
+        
+        if User.objects.filter(email=self.validated_data["email"]).exists():
+            raise serializers.ValidationError({'error':"User with Email Already Exists!"})
+        
+        
