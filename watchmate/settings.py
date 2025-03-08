b7d17c339d1f19737afec6e176f73558dab4bcb8
awais3344 <awaisalishah02@gmail.com>
2025-03-09 01:17:03 +0500

    DateTime Bug fixes

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-from datetime import timedalta
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -146,8 +146,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT={
     'ROTATE_REFRESH_TOKENS':True,
-    "ACCESS_TOKEN_LIFETIME":timedalta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME":timedalta(days=1)
+    "ACCESS_TOKEN_LIFETIME":timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME":timedelta(days=1)
 }
 
 
