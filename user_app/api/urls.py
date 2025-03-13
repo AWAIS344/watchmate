6048b54f0677c448e3d8d3a1a5ede94628773f2a
awais3344 <awaisalishah02@gmail.com>
2025-03-13 14:03:11 +0500

    Test case Addition

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 urlpatterns = [
     path("login/", obtain_auth_token,name="login"),
     path('register/',UserRegistration_View,name='register'),
-    path('logout/',UserLogout_View,name='register'),
+    path('logout/',UserLogout_View,name='logout'),
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- user_app/tests.py
+++ user_app/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from django.urls import reverse
+from rest_framework import status
 # Create your tests here.
+class RegistrationTestCase(APITestCase):
+
+    def test_register(self):
+        data={
+            "username":"test1",
+            "email":"test@gmail.com",
+            "password":"test123",
+            "password2":"test123"
+        }
+
+        response = self.client.post( reverse("register") ,data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
