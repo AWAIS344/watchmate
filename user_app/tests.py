e2db71ddf696d37f1733ca127f283c98859404a2
awais3344 <awaisalishah02@gmail.com>
2025-03-14 00:44:08 +0500

    Test case Addition

--- user_app/tests.py
+++ user_app/tests.py
@@ -17,3 +17,31 @@ class RegistrationTestCase(APITestCase):
 
         response = self.client.post( reverse("register") ,data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+
+class LoginLogoutTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='example',password="example12")
+        self.token = Token.objects.create(user=self.user)
+
+    def test_Login(self):
+
+        data={
+            'username':"example",
+            "password":"example12"
+        }
+
+        response=self.client.post(reverse('login'),data)
+
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+    def test_logout(self):
+
+        # self.token = Token.objects.get(user__username='example')
+        self.client.force_authenticate(user=self.user)
+        
+        # self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response=self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
